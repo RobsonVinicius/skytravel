a5db3fabd3d285b7248fbae1646ea65d7e8f8189
Robson <robsondgn@outlook.com>
2021-07-26 17:08:20 -0300

    Hidden Menu when click on link

--- script.js
+++ script.js
@@ -16,3 +16,11 @@ if(navClose) {
   })
 }
 
+// Hidden menu when click on link
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+  navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
