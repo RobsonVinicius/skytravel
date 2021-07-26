9805e1a5fb0dd9ccd3c206971216f85d334fa1da
Robson <robsondgn@outlook.com>
2021-07-26 13:56:00 -0300

    Header and Nav Menu (script show and hidden nav Toggle)

---
+++ script.js
@@ -0,0 +1,18 @@
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+// Show Menu
+if(navToggle){
+  navToggle.addEventListener('click', () => {
+      navMenu.classList.add('show-menu')
+  })
+}
+
+// Hidden Menu
+if(navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
+
