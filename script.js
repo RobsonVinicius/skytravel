992fa2d996c7b926c5a00f2e1b29e41afad23802
Robson <robsondgn@outlook.com>
2021-08-04 17:42:36 -0300

    Scroll Up / CSS / Script

--- script.js
+++ script.js
@@ -73,3 +73,15 @@ function finalVideo() {
 }
 
 videoFile.addEventListener('ended', finalVideo)
+
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+
+  if(this.scrollY >= 200) {
+    scrollUp.classList.add('show-scroll');
+  } else {
+    scrollUp.classList.remove('show-scroll')
+  }
+}
+
+window.addEventListener('scroll', scrollUp)
