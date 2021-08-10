e68437eed2ad765c9549bb9a97898fa8a8bf1417
Robson <robsondgn@outlook.com>
2021-08-10 02:01:03 -0300

    ScrollReveal

--- script.js
+++ script.js
@@ -132,3 +132,27 @@ themeButton.addEventListener('click', () => {
   localStorage.setItem('selected-icon', getCurrentIcon())
 
 })
+
+const sr = ScrollReveal({
+  distance: '60px',
+  duration: 2800,
+  // reset: true,
+})
+
+sr.reveal(`.home__data, .home__social-link, .home__info, 
+  .discover__container, .experience__data, .experience__overlay,
+  .place__card, .sponsor__content, .footer__data, .footer__rights`, {
+  origin: 'top',
+  internal: 100,
+}) 
+
+sr.reveal(`.about__data,
+  .video__description,
+  .subscribe__description`, {
+  origin: 'left',  
+}) 
+
+sr.reveal(`.about__img-overlay,
+  .video__content, .subscribe__form`, {
+  origin: 'right',  
+}) 
